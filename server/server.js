25874a58667991fa57e73cca202bc19210d44b6c
Jahidur Rahman Opu <jahidurrahmanopu3527@gmail.com>
2026-08-25 14:46:14 +0600

    fix: update CORS for production

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://task-manager-jet-eta.vercel.app", "http://localhost:5173"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
